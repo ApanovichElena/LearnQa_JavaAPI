fd88911e31b1ec10dbd6401f14a572a08d07b513
Elena Apanovich <apanovich.ellen@gmail.com>
2022-09-18 20:54:16 +0300

    HelloWordTest

---
+++ src/test/java/HelloWordTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWordTest {
+
+    @Test
+    public void testHelloWord() {
+        System.out.println("Hello_word");
+                                 }
+}
